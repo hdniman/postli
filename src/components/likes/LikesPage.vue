ab2a4ff98624f8121e82607666b71af69408f640
dima <dem0noid@gmail.com>
2021-10-24 23:05:24 +0300

    ended rating

--- src/components/likes/LikesPage.vue
+++ src/components/likes/LikesPage.vue
@@ -3,13 +3,39 @@
     <h1>
       likes
     </h1>
+    <rated-item v-for="ratedItem in ratingData" :key="ratedItem" :ratedItem="ratedItem"></rated-item>
+    <h3 v-if="ratingData.length == 0">You haven't liked anything yet!</h3>
   </div>
 </template>
 
 <script>
+import RatedItem from "./RatedItem.vue";
+import { ref } from '@vue/reactivity'
+import { useStore } from 'vuex'
 export default {
   setup() {
+    const store = useStore()
+
+    const userData = ref(store.getters['auth/userData'])
+    if (userData.value.rating == undefined) {
+      userData.value.rating = {}
+    }
+    if (userData.value.rating.likes == undefined) {
+      userData.value.rating.likes = {}
+    }
+    if (userData.value.rating.dislikes == undefined) {
+      userData.value.rating.dislikes = {}
+    }
     
+    const ratingData = ref(Object.keys(userData.value.rating.likes))
+
+
+    return {
+      ratingData
+    }
+  },
+  components: {
+    RatedItem
   }
 
 }
---
+++ src/components/likes/RatedItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-link custom v-slot="{ navigate }" :to="`/post/${ratedItem}`">
+    <div class="card" @click="navigate">
+      <h3>{{postData.title}}</h3>
+      <h4>{{postData.description}}</h4>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import { getPostData } from '../../use/getData'
+export default {
+  props: ['ratedItem'],
+  setup(props) {
+    const postData = getPostData(props.ratedItem)
+
+    return {
+      postData
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
